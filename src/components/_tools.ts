a871ae645b21c73c1898efd39c47f0bffe0c54a1
Clembs <clembs@clembs.com>
2025-07-19 19:54:50 +0200

    feat: purplet & discord tools

--- src/components/_tools.ts
+++ src/components/_tools.ts
@@ -1,19 +1,23 @@
-import {
-  CSS,
-  Firebase,
-  Flutter,
-  HTML,
-  PostgreSQL,
-  Prisma,
-  Sass,
-  Svelte,
-  TypeScript,
-} from "@assets/icons/tools";
+import * as CSS from "@assets/icons/tools/css.svg?raw";
+import * as Discord from "@assets/icons/tools/discord.svg?raw";
+import * as Firebase from "@assets/icons/tools/firebase.svg?raw";
+import * as Flutter from "@assets/icons/tools/flutter.svg?raw";
+import * as HTML from "@assets/icons/tools/html.svg?raw";
+import * as PostgreSQL from "@assets/icons/tools/postgresql.svg?raw";
+import * as Prisma from "@assets/icons/tools/prisma.svg?raw";
+import * as Purplet from "@assets/icons/tools/purplet.svg?raw";
+import * as Sass from "@assets/icons/tools/sass.svg?raw";
+import * as Svelte from "@assets/icons/tools/svelte.svg?raw";
+import * as TypeScript from "@assets/icons/tools/typescript.svg?raw";
 
 export default [
   {
     name: "CSS",
     icon: CSS,
+  },
+  {
+    name: "Discord",
+    icon: Discord,
   },
   {
     name: "Firebase",
@@ -26,6 +30,10 @@ export default [
   {
     name: "HTML",
     icon: HTML,
+  },
+  {
+    name: "Purplet",
+    icon: Purplet,
   },
   {
     name: "PostgreSQL",
