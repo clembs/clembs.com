37afc4a12fa237a9f2534137f7cbbf22b98acbf5
Clément Voisin <clembs@clembs.com>
2025-08-14 00:36:30 +0200

    refactor: simplify blog schema & load layout

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,26 +1,27 @@
-import { defineCollection, z, type ImageFunction } from "astro:content";
+import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
-
-export const BlogSchema = (image: ImageFunction) =>
-  z.object({
-    title: z.string(),
-    summary: z.string(),
-    createdAt: z.coerce.date(),
-    lastEditedAt: z.coerce.date().optional(),
-    category: z.string(),
-    bannerPath: image().optional(),
-    bannerThumbnailPath: z.string().optional(),
-    bannerAlt: z.string().optional(),
-    projectUrl: z.string().optional(),
-    clientText: z.string().optional(),
-  });
+import type { CollectionEntry } from "astro:content";
 
 const blog = defineCollection({
   loader: glob({
     pattern: "**/*.mdx",
     base: "./src/content/blog/",
   }),
-  schema: ({ image }) => BlogSchema(image),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      summary: z.string(),
+      createdAt: z.coerce.date(),
+      lastEditedAt: z.coerce.date().optional(),
+      category: z.string(),
+      bannerPath: image().optional(),
+      bannerThumbnailPath: z.string().optional(),
+      bannerAlt: z.string().optional(),
+      projectUrl: z.string().optional(),
+      clientText: z.string().optional(),
+    }),
 });
 
 export const collections = { blog };
+
+export type BlogData = CollectionEntry<"blog">["data"];
